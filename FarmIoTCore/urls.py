3623fd373ad8274e77c61b0dc3bb79af976c91e4
banworddd <i.want.magiccc@gmail.com>
2025-04-14 13:54:56 +0300

    minor fixes in dashboard models, add DRF, update requirements

--- FarmIoTCore/urls.py
+++ FarmIoTCore/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', include('main.urls')),
     path('', include('users.urls')),
